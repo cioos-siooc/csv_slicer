92ef857784a786e1797ca0d9e63fb448a66c9df7
Scott Bruce <scott.bruce@mi.mun.ca>
2021-04-26 15:29:28 -0230

    added JSON-based formatting and labelling options, dropped writing index from output
    Formatting options are something of a preprocessing option to rework the output of columns prior to partitioning the data into separate files.  Essentially to enforce consistency in the partitioning values.
    
    The labels option is to allow for the option of adding column names to the output files for specifically matching partition values/files.

--- csv_partition.py
+++ csv_partition.py
@@ -10,17 +10,35 @@ multiple kinds of messages, like Campbell Scientific CSV.
 import os
 import argparse
 import pandas as pd
+import json
 from pathlib import Path
 
 def main(prog_args):
     output_files = {}
 
     source = open(prog_args.source_file.strip(), "r")
-    index_col = prog_args.column
+    partition_column = prog_args.column
+    
+    try:
+        formats = json.loads(prog_args.format.strip())
+    except AttributeError:
+        formats = {}
+
+    try:
+        labels = json.loads(prog_args.labels)
+    except TypeError:
+        labels = {}
+    
 
     for line in source:
         row = line.strip().split(",")
-        column = row[index_col]
+        
+        # https://stackoverflow.com/questions/20003290/output-different-precision-by-column-with-pandas-dataframe-to-csv
+        for column, format_str in formats.items():
+            col_int = int(column)
+            row[col_int] = prep_value(value=row[col_int], format_str=format_str)
+
+        column = row[partition_column]
 
         if column in output_files:
             output_files[column].append(row)
@@ -38,7 +56,20 @@ def main(prog_args):
             os.makedirs(os.path.dirname(output_path))
 
         df = pd.DataFrame.from_dict(output_files[partition])
-        df.to_csv(output_path)
+
+        headers = False
+        if labels and partition in labels:
+            headers = labels[partition].split(",")
+
+        df.to_csv(output_path, index=False, header=headers)
+
+def prep_value(value, format_str):
+    if format_str['type'] == 'int':
+        value = int(value)
+    elif format_str['type'] == 'float':
+        value = float(value)
+
+    return format_str['output'].format(value)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -55,6 +86,21 @@ if __name__ == "__main__":
         help="Column that contains unique values to partition into sepearate files",
         default=0,
         type=int,
+        action="store",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--format",
+        # Inspired by this answer https://stackoverflow.com/a/62546734
+        help="A JSON formatted list of columns (zero-based) and how they should be formatted according to python string formatting - applies to ALL rows before partitioning.\r\n  Example: {\"0\":{\"type\":\"float\",\"output\":\"{:1.2f}\"}} would format the value in the first column to 2 decimal places.",
+        action="store",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--labels",
+        help="A JSON formatted list of column labels coresponding to 1 or more of the values related to the partition column. Example: {\"value1\":[\"col_a\",\"col_b\", \"col_c\"], ... ,\"valueN\":[\"label_a\",\"label_b\"]}",
         action="store",
     )
 
